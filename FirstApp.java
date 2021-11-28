d85981f664a63a3980edb5dd8ffa7ab44ffd2b87
naissur <daily.spb@yandex.ru>
2021-11-29 00:32:08 +0300

    Print 3 lines

--- FirstApp.java
+++ FirstApp.java
@@ -1,5 +1,9 @@
 public class FirstApp {
   public static void main(String[] args) {
     System.out.println("Hello, this is a first application");
+
+    for (int i = 1; i <= 3; i++) {
+      System.out.println("Line " + i);
+    }
   }
 }
